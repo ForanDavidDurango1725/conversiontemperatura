568f97307f98083322ab3446ade72880edede76d
ForánDavidDurango1725 <forand.d305@gmail.com>
2023-09-04 23:44:42 -0500

    Agregar archivos de proyecto.

---
+++ conversiontemperatura/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversiontemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }using System;
+
+class ConversionTemperatura
+    {
+        static void Main()
+        {
+            double celsius, fahrenheit;
+
+            Console.Write("Ingrese una temperatura en grados Celsius: ");
+            celsius = double.Parse(Console.ReadLine());
+
+            fahrenheit = celsius * 9 / 5 + 32;
+
+            Console.WriteLine("La temperatura equivalente en grados Fahrenheit es: {0}", fahrenheit);
+        }
+    }
+}
+}
